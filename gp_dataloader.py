527b28d0ad97ec26df4453cec786f6ca6c72f62d
Thomas <thomas.coste18@imperial.ac.uk>
2023-02-16 21:55:22 +0000

    Extend GP dataloader to randomizing kernel parameters

--- gp_dataloader.py
+++ gp_dataloader.py
@@ -1,21 +1,33 @@
 import numpy as np
 import torch
 
-from sklearn.gaussian_process.kernels import Matern
+from sklearn.gaussian_process.kernels import Matern, RBF, WhiteKernel, ConstantKernel
 from sklearn.gaussian_process import GaussianProcessRegressor
 
 
-# Values from original experiment
+# Original CNP params
 MIN_X = -2
 MAX_X = 2
 MIN_N_CONTEXT = 3
 MAX_N_CONTEXT = 100
-LENGTH_SCALE = 0.6
+LENGTH_SCALE = 0.4
 TEST_N_TARGET = 400
 BATCH_SIZE = 16
+NOISE_LEVEL = 2e-2**2
+
+# ANP random kernel bounds
+MIN_KERNEL_SCALE = 0.1
+MAX_KERNEL_SCALE = 1.0
+MIN_LENGTH_SCALE = 0.1
+MAX_LENGTH_SCALE = 0.6
+DEFAULT_KERNEL = (
+    RBF(LENGTH_SCALE, (MIN_LENGTH_SCALE, MAX_LENGTH_SCALE)) + WhiteKernel(NOISE_LEVEL)
+) * ConstantKernel(1.0, (MIN_KERNEL_SCALE, MAX_KERNEL_SCALE))
 
 
 class NPTuple(object):
+    """Tuple of arrays served as input to neural processes"""
+
     def __init__(self, x_context, y_context, x_target, y_target):
         self.x_context = x_context
         self.y_context = y_context
@@ -40,20 +52,32 @@ class NPTuple(object):
 
 
 class GPDataGenerator(object):
+    """Generates batches of data from a Gaussian Process"""
+
     def __init__(
         self,
         testing=False,
-        length_scale=0.6,
+        kernel=None,
+        randomize_kernel_params=False,
         max_n_context=MAX_N_CONTEXT,
         batch_size=BATCH_SIZE,
     ):
         self.testing = testing
         self.max_n_context = max_n_context
-        self.kernel = Matern(length_scale=length_scale)
-        self.gp = GaussianProcessRegressor(kernel=self.kernel)
+        kernel = kernel or (
+            RBF(LENGTH_SCALE, (MIN_LENGTH_SCALE, MAX_LENGTH_SCALE))
+            + WhiteKernel(NOISE_LEVEL, "fixed")
+        ) * ConstantKernel(1.0, (MIN_KERNEL_SCALE, MAX_KERNEL_SCALE))
+        if not randomize_kernel_params:
+            for p in kernel.hyperparameters:
+                kernel.set_params(**{f"{p.name}_bounds": "fixed"})
+        self.gp = GaussianProcessRegressor(kernel=kernel)
+        self.randomize_kernel_params = randomize_kernel_params
         self.batch_size = batch_size
 
     def generate_batch(self):
+        """Generate a batch of samples from the GP"""
+
         n_context = np.random.randint(MIN_N_CONTEXT, self.max_n_context + 1)
 
         # Evenly distributed x values at test time
@@ -65,10 +89,9 @@ class GPDataGenerator(object):
                 .repeat(self.batch_size, 1)
                 .unsqueeze(-1)
             )
-
         # Random x values at train time
         else:
-            n_target = np.random.randint(1, self.max_n_context - n_context + 1)
+            n_target = np.random.randint(0, self.max_n_context - n_context + 1)
             x_values = (
                 torch.rand(self.batch_size, n_context + n_target, 1) * (MAX_X - MIN_X)
                 + MIN_X
@@ -78,6 +101,9 @@ class GPDataGenerator(object):
         targets = []
         x_values, _ = torch.sort(x_values, dim=1)
         for i in range(self.batch_size):
+            # Randomize kernel if needed for experiment
+            if self.randomize_kernel_params:
+                self.randomize_k_params()
             targets.append(torch.from_numpy(self.gp.sample_y(x_values[i], n_samples=1)))
         targets = torch.stack(targets).squeeze(-1)
 
@@ -94,3 +120,10 @@ class GPDataGenerator(object):
         y_target = targets
 
         return NPTuple(x_context, y_context, x_target, y_target)
+
+    def randomize_k_params(self):
+        """Randomize kernel parameters by sampling from their bounds"""
+        kernel = self.gp.kernel
+        for p in kernel.hyperparameters:
+            if not isinstance(p.bounds, str):
+                kernel.set_params(**{p.name: np.random.uniform(*p.bounds.squeeze())})
