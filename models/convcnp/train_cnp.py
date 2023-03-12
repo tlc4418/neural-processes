9a16770209f2841ef5ed65739602ae87f6bde6a7
Bryan <56035353+zcahudx@users.noreply.github.com>
2023-03-12 16:12:40 +0000

    Add files via upload

--- models/convcnp/train_cnp.py
+++ models/convcnp/train_cnp.py
@@ -1,4 +1,6 @@
 import torch
+from pathlib import Path
+from datetime import datetime
 import os
 import random
 import numpy as np
@@ -17,7 +19,6 @@ plot_freq= 10000
 
 def train_1d(
     model,
-    model_name,
     epochs=10000,
     train_gen=GPDataGenerator(),
     test_gen=GPDataGenerator(testing=True, batch_size=1),
@@ -26,6 +27,9 @@ def train_1d(
     print(f"Using device: {device}")
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    NLL = []
+    Path("checkpoint/{}".format(timestamp)).mkdir(parents=True, exist_ok=True)   
 
     for epoch in range(epochs):
         model.train()
@@ -36,27 +40,21 @@ def train_1d(
         loss.backward()
         optimizer.step()
 
-        if epoch % plot_freq == 0 or (epoch+1)==epochs:
+        if (epoch+1) % plot_freq == 0 or (epoch+1)==epochs:
             model.eval()
             with torch.no_grad():
-              context_x, context_y, target_x, target_y = prepare_data(test_gen, device)
-              pred_y, std, loss= model(context_x, context_y, target_x, target_y)
+                context_x, context_y, target_x, target_y = prepare_data(test_gen, device)
+                pred_y, std, loss= model(context_x, context_y, target_x, target_y)
             
-              plot_np_results(
+                plot_np_results(
                   *prepare_plot([target_x, target_y, context_x, context_y, pred_y, std]),
-                  title=f"Epoch: {epoch} Loss: {loss.item():.4f}",
+                   title=f"Epoch: {epoch} Loss: {loss.item():.4f}",
               )
-            
-
-            os.makedirs("/content/drive/MyDrive/neural-processes-main/models/convcnp/1d_saved", exist_ok=True)
-              
-            torch.save(
-                {
-                    "model_state_dict": model.state_dict(),
-                    "optimizer_state_dict": optimizer.state_dict(),
-                },
-                "/content/drive/MyDrive/neural-processes-main/models/convcnp/1d_saved/"+"convcnp_model"+"_"+model_name+"_"+str(epoch)+".pt",
-            )
+                model_path = "checkpoint/{}/model_{}".format(timestamp, epoch + 1) 
+                torch.save(
+                 model.state_dict(),
+                 model_path
+              )
 
 
 def prepare_data(generator, device):
@@ -77,7 +75,6 @@ def prepare_plot(objects):
 
 def train_2d(
     model,
-    model_name,
     train_loader,
     val_loader,
     a,
@@ -88,7 +85,7 @@ def train_2d(
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
     model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
     masker = GetBatchMask(a=a, b=b)
     def train_one_epoch(epoch):
         running_loss = 0.0
@@ -110,7 +107,10 @@ def train_2d(
                 tb_x = epoch * len(train_loader) + idx + 1
                 running_loss = 0.0
         return last_loss
-            
+    
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    NLL = []
+    Path("checkpoint/{}".format(timestamp)).mkdir(parents=True, exist_ok=True)      
     best_val_loss = 1e20
     for epoch in range(epochs):
         
@@ -128,72 +128,10 @@ def train_2d(
                 mask = mask.to(device)
                 pred_y_val, std_val, val_loss = model(data, mask)
                 running_val_loss += val_loss
-            
-                if idx==0:
-                    if data.shape[1]==1:
-                        query = random.randint(0, data.shape[0])
-                        img = data[query].squeeze(0)
-                        img = img.detach().cpu()
-                        single_mask = mask[query].squeeze(0)
-                        single_mask = single_mask.detach().cpu()
-                        masked_img = img.clone()
-                        masked_img[single_mask==0.0] = np.NaN
-                        predicted_img = pred_y_val[query].squeeze(-1)
-                        predicted_img = predicted_img.detach().cpu()
-                        plt.figure()
-
-                        f, axe = plt.subplots(1,3) 
-                        axe[0].imshow(img,cmap="gray")
-                        axe[0].xaxis.set_tick_params(labelbottom=False)
-                        axe[0].yaxis.set_tick_params(labelleft=False)
-                        axe[0].set_xticks([])
-                        axe[0].set_yticks([])
-                        axe[1].imshow(masked_img,cmap="gray")
-                        axe[1].xaxis.set_tick_params(labelbottom=False)
-                        axe[1].yaxis.set_tick_params(labelleft=False)
-                        axe[1].set_xticks([])
-                        axe[1].set_yticks([])
-                        axe[1].patch.set_facecolor('blue')
-                        axe[2].imshow(predicted_img,cmap="gray")
-                        axe[2].xaxis.set_tick_params(labelbottom=False)
-                        axe[2].yaxis.set_tick_params(labelleft=False)
-                        axe[2].set_xticks([])
-                        axe[2].set_yticks([])
-                    
-                    else:
-                        query = random.randint(0, data.shape[0])
-                        img = data[query].permute(1,2,0)
-                        img = img.detach().cpu()
-                        single_mask = mask[query].permute(1,2,0)
-                        single_mask = single_mask.detach().cpu()
-                        masked_img = img.clone()
-                        masked_img[(single_mask.expand_as(img))==0.0] = np.NaN
-                        predicted_img = pred_y_val[query]
-                        predicted_img = predicted_img.detach().cpu()
-                        plt.figure()
-
-                        f, axe = plt.subplots(1,3) 
-                        axe[0].imshow(img)
-                        axe[0].xaxis.set_tick_params(labelbottom=False)
-                        axe[0].yaxis.set_tick_params(labelleft=False)
-                        axe[0].set_xticks([])
-                        axe[0].set_yticks([])
-                        axe[1].imshow(masked_img)
-                        axe[1].xaxis.set_tick_params(labelbottom=False)
-                        axe[1].yaxis.set_tick_params(labelleft=False)
-                        axe[1].set_xticks([])
-                        axe[1].set_yticks([])
-                        axe[1].patch.set_facecolor('black')
-                        axe[2].imshow(predicted_img)
-                        axe[2].xaxis.set_tick_params(labelbottom=False)
-                        axe[2].yaxis.set_tick_params(labelleft=False)
-                        axe[2].set_xticks([])
-                        axe[2].set_yticks([])
-                    
-            
         
             avg_val_loss = running_val_loss/len(val_loader)
         
+        NLL.append(avg_training_loss)
         print(
             "LOSS train {:.5f} valid {:.5f}".format(
                 avg_training_loss, avg_val_loss
@@ -201,18 +139,45 @@ def train_2d(
         )
         
         
-        if avg_val_loss < best_val_loss:
-            best_val_loss = avg_val_loss
+        # if avg_val_loss < best_val_loss:
+        #     best_val_loss = avg_val_loss
         
-            os.makedirs("/content/drive/MyDrive/neural-processes-main/models/convcnp/2d_saved", exist_ok=True)
-              
-            torch.save(
-               {
-                   "model_state_dict": model.state_dict(),
-                   "optimizer_state_dict": optimizer.state_dict(),
-               },
-               "/content/drive/MyDrive/neural-processes-main/models/convcnp/2d_saved/"+"gridconvcnp_model"+"_"+model_name+"_"+str(epoch+1)+".pt",
-           )
-            model_path =  "/content/drive/MyDrive/neural-processes-main/models/convcnp/2d_saved/"+"gridconvcnp_model"+"_"+model_name+"_"+str(epoch+1)+".pt"
+        # os.makedirs("/content/drive/MyDrive/neural-processes-main/models/convcnp/2d_saved", exist_ok=True)
+        model_path = "checkpoint/{}/model_{}".format(timestamp, epoch + 1) 
+        torch.save(
+             model.state_dict(),
+             model_path
+        )
+        
+    plt.plot(NLL)
+    plt.xlabel("epoch")
+    plt.ylabel("NLL")
+    plt.savefig("plots/GridConvCNP_Celeba_NLL")
     
     return model_path
+
+def test_2d(model, a, b,  test_loader):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    model.to(device)
+    model.train(False)
+    running_test_loss = 0.0
+    masker = GetBatchMask(a=a, b=b)
+    with torch.no_grad():
+        for idx, (data, _) in enumerate(test_loader):
+                
+            data=data.to(device)
+            mask = masker(data.shape[0], (data.shape[2], data.shape[3]), is_same_mask=True)
+            mask = mask.to(device)
+            pred_y_test, std_test, test_loss = model(data, mask)
+            running_test_loss += test_loss
+    
+    avg_test_loss = running_test_loss/len(test_loader)
+    
+    print(
+            "LOSS test {:.5f}".format(
+                avg_test_loss
+            )
+        )
+    
+    return avg_test_loss
